64068ce131bd443f56897eade5d8d6d89f910121
nahueldiaz743-commits <nahuel.diaz743@gmail.com>
2026-05-12 15:43:06 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bar-a-v3';
+const FILES=['./index_v2.html','./manifest.json','./logo.jpg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index_v2.html')))));
