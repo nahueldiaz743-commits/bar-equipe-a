fbcfb8679a55a1a340e19a351c0476b3375578ec
nahueldiaz743-commits <nahuel.diaz743@gmail.com>
2026-05-12 15:42:15 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='bar-a-v2';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index_v2.html','./manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index_v2.html')))));
